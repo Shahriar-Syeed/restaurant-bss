f7d49f881769f2942cf5455a0c1863bc49e62274
Mohammad Shahriar <128366372+Shahriar-Syeed@users.noreply.github.com>
2026-09-24 19:10:42 +0600

    Modify user ID condition in requireAuth
    Update user ID check in requireAuth function.

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -25,7 +25,7 @@ const requireAuth = async () => {
   const user = await JSON.parse(sessionStorage.getItem("user"));
   const saveToken = sessionStorage.getItem("token");
   if (
-    user?.id === "eb87aaa2-bf85-48d5-56a4-08d906dd12b1" &&
+    user?.id === "a080b321-b765-450e-9ab7-6a8a47864006" &&
     saveToken?.startsWith("Bearer")
   ) {
     return null;
